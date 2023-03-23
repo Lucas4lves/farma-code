e51e80f6bd25502e1f98bb759f6e0a341637c678
Mateus Azevedo <mateusazevedoaguiar@gmail.com>
2023-03-23 10:42:42 -0300

    calculo de distancia

--- src/controllers/DistanciaController.js
+++ src/controllers/DistanciaController.js
@@ -1,55 +1,60 @@
 const lojaModel = require("../models/Loja")
 
-module.exports = class DistanciaController
-{
-    static async calcularDistancia(req, res)
-    {   
-        let {localizacao_cliente, lista_remedios} = req.body;
+module.exports = class DistanciaController {
+    static async calcularDistancia(req, res) {
+        let { localizacao_cliente, lista_remedios } = req.body;
 
         let lojas = await lojaModel.find();
-        if(!lojas)
-        {
+        if (!lojas) {
             return res.status(400).json({
                 error: true,
                 msg: "Não foi possível encontrar lojas"
             })
         }
-
+        
         const lojasDisponiveis = lojas => lojas.map(loja => {
+            let resultado = checarEstoque(lista_remedios, loja.estoque); 
+            let {matches, encontrados} = resultado;
             return {
                 ...loja,
-                matches: checarEstoque(lista_remedios, loja.estoque)
+                matches: matches,
+                encontrados: encontrados
             }
         })
 
-        const distancia = (melhoresLojas, localizacao_cliente) => melhoresLojas.map(loja => {
+        console.log(lojasDisponiveis);
+        
+        const distancia = (melhoresLojas, localizacao_cliente_func) => melhoresLojas.map(loja => {
             return {
                 ...loja,
-                dist: distance(Number(loja.latitude), Number(loja.longitude), Number(localizacao_cliente.latitude), Number(localizacao_cliente.longitude))
+                dist: distance(Number(loja._doc.latitude), Number(loja._doc.longitude), Number(localizacao_cliente_func.latitude), Number(localizacao_cliente_func.longitude))
             }
         })
 
         let l = lojasDisponiveis(lojas);
 
+        console.log(l);
+        
         let melhoresLojas = l.filter(loja => loja.matches >= lista_remedios.length - 1);
+
+        console.log("melhores lojas", melhoresLojas);
 
         return res.status(200).json({
             encontrado: true,
-            lojas: distancia(melhoresLojas, localizacao_cliente)
+            lojas: distancia(melhoresLojas,localizacao_cliente)
         })
 
-        function checarEstoque(lista_med, estoque)
-        {
+        function checarEstoque(lista_med, estoque) {
             let matches = 0;
-            for(let i = 0; i < lista_med.length; i++)
-            {
-                if(estoque.includes(lista_med[i]))
-                {
+            let encontrados = []
+            for (let i = 0; i < lista_med.length; i++) {
+                if (estoque.includes(lista_med[i])) {
                     matches = matches + 1;
+                    encontrados.push(lista_med[i])
                 }
             }
 
-            return matches;
+            return {matches,encontrados};
         }
 
         function distance(lat1, lon1, lat2, lon2) {
@@ -57,19 +62,20 @@ module.exports = class DistanciaController
             const dLat = toRadians(lat2 - lat1);
             const dLon = toRadians(lon2 - lon1);
             const a =
-              Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-              Math.cos(toRadians(lat1)) *
+                Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(toRadians(lat1)) *
                 Math.cos(toRadians(lat2)) *
                 Math.sin(dLon / 2) *
                 Math.sin(dLon / 2);
             const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
             const d = R * c; // distância em km
-            return d;
-          }
-          
-          function toRadians(degrees) {
-            return (degrees * Math.PI) / 180;
-          }
+            console.log([lat1, lon1, lat2, lon2]);
+            return d.toFixed(2);
+        }
 
-    }   
+        function toRadians(degrees) {
+            return (degrees * Math.PI) / 180;
+        }
+
+    }
 }
